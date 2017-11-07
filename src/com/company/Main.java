d2a3687f75cb72bd1e9856a7c2127e57fd1cd26f
sxoxs <33289095+sxoxs@users.noreply.github.com>
2017-11-07 11:34:46 +0300

    испр, в соотв с замечаниями

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,35 +4,37 @@ import java.util.Random;
 
 public class Main {
 
+    public static final String STR_TEXT = "Съешь ещё этих мягких французских булок, да выпей же чаю";
+
     public static void main(String[] args) {
+
         System.out.println("Часть 1");
-        outRandomRowsInt(20);
+        outRandomRowsInt(20, 1, 100);
         System.out.println("\nЧасть 2");
-        outRandomCol(10);
+        outRandomCol(10, 25, 75);
         System.out.println("\nЧасть 3");
-        String strText= "Съешь ещё этих мягких французских булок, да выпей же чаю";
-        outStringFromSpace(strText);
+        outStringFromSpace(STR_TEXT);
         System.out.println("\nРабота завершена");
        }
 
-    static void outRandomRowsInt (int count) {
+    static void outRandomRowsInt (int count, int min, int max) {
         Random rand = new Random();
 
-        System.out.print("[");
+        System.out.print("{");
         for (int i = 1; i <= count; i++){
-            System.out.print(rand.nextInt(100) + 1);
+            System.out.print(rand.nextInt(max) + min);
             if (i != count) {
                 System.out.print(" ");
             }
-            else System.out.println("]");
+            else System.out.println("}");
         }
     }
 
-    static void outRandomCol (int count) {
+    static void outRandomCol (int count, int min, int max) {
         Random rand = new Random();
 
         for (int i = 1; i <= count; i++) {
-            System.out.printf("%.3f %n", (rand.nextDouble() + rand.nextInt(49) + 25));
+            System.out.printf("%.3f %n", (rand.nextDouble() + rand.nextInt(max) + min));
         }
     }
 
